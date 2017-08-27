6bcf13de8b1ad2e351e2fbd2db617fc34253ecdc
Bartosz Rozinski <bartosz.rozinski@gmail.com>
2017-08-27 22:39:21 +0200

    Visible + loops

--- WindowsFormsBasicApp/Form1.cs
+++ WindowsFormsBasicApp/Form1.cs
@@ -19,18 +19,21 @@ namespace WindowsFormsBasicApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for (int c = 0; c < 253; c++)
+            while (Visible)
             {
-                this.BackColor = Color.FromArgb(c, 225 - c, c);
-                Application.DoEvents();
-                System.Threading.Thread.Sleep(3);
+                for (int c = 0; c < 253 && Visible; c++)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
 
-            }
-            for (int c = 253; c < 254; c --)
-            {
-                this.BackColor = Color.FromArgb(c, 225 - c, c);
-                Application.DoEvents();
-                while (Visible == true) ;
+                }
+                for (int c = 254; c > 0 && Visible; c--)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
             }
         }
     }
